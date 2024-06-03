ec80f079d0296384654e3e19068cb96a58a5faf7
PB2 <nft192891@gmail.com>
2024-06-03 22:06:33 +0800

    建置後台主要功能項目頁面

--- back/th.php
+++ back/th.php
@@ -1,1 +1,66 @@
-商品分類與管理
+<h2 class="ct">商品分類</h2>
+<div class="ct">
+    新增大分類 
+    <input type="text" name="big" id="big"> <button onclick="">新增</button>
+</div>
+<div class="ct">
+    新增中分類 
+    <select name="bigtype" id="bigtype">
+        <option value="">流行皮件</option>
+    </select>
+    <input type="text" name="mid" id="mid">
+    <button onclick="">新增</button>
+</div>
+
+
+
+<table class="all">
+    <tr class="tt">
+        <td>皮件</td>
+        <td>
+            <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+    <tr class="pp">
+        <td>女用皮件</td>
+        <td>
+            <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+</table>
+
+
+<h2 class="ct">商品分類</h2>
+<div class="ct">
+    <button onclick="">新增商品</button>
+</div>
+
+<select name="" id="">
+    <option value="">全部商品</option>
+</select>
+
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>庫存量</td>
+        <td>狀態</td>
+        <td>操作</td>
+    </tr>
+    <tr class="pp ct">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>
+            <button onclick="">修改</button>
+            <button onclick="">刪除</button><br>
+            <button onclick="">上架</button>
+            <button onclick="">下架</button>
+        </td>
+    </tr>
+</table>
+
+
