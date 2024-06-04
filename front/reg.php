6225f1ee79982d9ebb995a1f906dc83bb8a2fede
PB2 <nft192891@gmail.com>
2024-06-04 23:08:08 +0800

    完成註冊會員功能

--- front/reg.php
+++ front/reg.php
@@ -39,11 +39,33 @@
     function chkAcc(){
         let acc=$('#acc').val()
         $.post('./api/chk_acc.php', {table:'classb_4_mem',acc}, (res)=>{ //除了帳號, 再多提供一個table指定為classb_4_mem, 供api判斷是去管理者還是使用者的table查詢
-            console.log(res);
+            // console.log(res);
             if(parseInt(res)===1 || acc==='admin'){
                 alert("帳號已被使用, 請選擇其他帳號")
             }else{
                 alert("此帳號可使用")
+            }
+        })
+    }
+
+
+    function reg(){
+        let user={
+            acc:$('#acc').val(),
+            pw:$('#pw').val(),
+            name:$('#name').val(),
+            addr:$('#addr').val(),
+            email:$('#email').val(),
+            tel:$('#tel').val()
+        }
+
+        $.post('./api/chk_acc.php',{acc:user.acc}, (res)=>{ // 先只傳帳號判斷
+            if(parseInt(res)===1 || user.acc==='admin'){
+                alert("帳號已被使用, 請選擇其他帳號")
+            }else{
+                $.post('./api/reg.php', user, (res)=>{ // 再傳整個表單存入
+                    location.href='index.php?do=login'
+                })
             }
         })
     }
