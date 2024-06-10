553de21a7f438e8a43339cc7d300a692cee84f44
PB2 <nft192891@gmail.com>
2024-06-10 10:22:15 -0500

    管理者登入功能

--- front/login.php
+++ front/login.php
@@ -25,34 +25,3 @@
 </table>
 <div class="ct"><button onclick="login('classb_4_mem')">確認</button></div>
 
-<script>
-    function login(table){
-        let ans=$("#code").val()
-        let user={
-            acc:$("#acc").val(),
-            pw:$("#pw").val(),
-            table
-        }
-        $.get("./api/ans.php",{ans},(res)=>{
-            if(parseInt(res)===1){
-                // alert("正確")
-                $.get("./api/login.php",user,(res)=>{
-                    if(parseInt(res)===1){
-                        switch(table){
-                            case 'classb_4_mem':
-                                location.href='index.php';
-                            break;
-                            case 'admin':
-                                location.href='back.php';
-                            break;
-                        }
-                    }else{
-                        alert("帳號或密碼錯誤")
-                    }
-                })
-            }else{
-                alert("驗證碼錯誤, 請重新輸入")
-            }
-        })
-    }
-</script>
--- js/js.js
+++ js/js.js
@@ -3,3 +3,34 @@ function lof(x)
 {
 	location.href=x
 }
+
+
+function login(table){
+	let ans=$("#code").val()
+	let user={
+		acc:$("#acc").val(),
+		pw:$("#pw").val(),
+		table
+	}
+	$.get("./api/ans.php",{ans},(res)=>{
+		if(parseInt(res)===1){
+			// alert("正確")
+			$.get("./api/login.php",user,(res)=>{
+				if(parseInt(res)===1){
+					switch(table){
+						case 'classb_4_mem':
+							location.href='index.php';
+						break;
+						case 'classb_4_admin':
+							location.href='back.php';
+						break;
+					}
+				}else{
+					alert("帳號或密碼錯誤")
+				}
+			})
+		}else{
+			alert("驗證碼錯誤, 請重新輸入")
+		}
+	})
+}
